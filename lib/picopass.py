458bb0839929481ab10c1f35025ea29bec64b77f
antruc <admin@antruc.dev>
2026-05-18 11:16:38 -0600

    Refactor code and improve comments

--- lib/picopass.py
+++ lib/picopass.py
@@ -1,22 +1,22 @@
-import time
 import board
 import digitalio
+import time
 import usb_hid
 
-# For English keyboard
+# English keyboard
 from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
 
-# For Spanish keyboard
+# Spanish keyboard
 # https://github.com/Neradoc/Circuitpython_Keyboard_Layouts
-# from keycode_win_es import Keycode
 # from keyboard_layout_win_es import KeyboardLayout
+# from keycode_win_es import Keycode
 
-import busio
-import displayio
 import adafruit_displayio_sh1107
 import adafruit_imageload
+import busio
+import displayio
 from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text import label
 
@@ -51,7 +51,7 @@ def write_text(text):
 # Release any currently configured displays
 displayio.release_displays()
 
-# Use for SPI
+# Configure SPI
 oled_clk = board.GP10
 oled_din = board.GP11
 oled_cs = board.GP9
@@ -63,13 +63,13 @@ display_bus = displayio.FourWire(
     spi, command=oled_dc, chip_select=oled_cs, reset=oled_res
 )
 
-# Resolution
+# Set Resolution
 WIDTH = 128
 HEIGHT = 64
 
 display = adafruit_displayio_sh1107.SH1107(display_bus, width=WIDTH, height=HEIGHT)
 
-# Make the display context
+# Make display context
 splash = displayio.Group()
 display.root_group = splash
 
@@ -80,7 +80,7 @@ color_palette[0] = 0x000000
 bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
 splash.append(bg_sprite)
 
-# Create icon
+# Load icon
 bitmap, palette = adafruit_imageload.load(
     "/icon.bmp", bitmap=displayio.Bitmap, palette=displayio.Palette
 )
@@ -95,28 +95,19 @@ splash.append(tile_grid)
 
 # Set font and color
 font = bitmap_font.load_font("/spleen-32x64.bdf")
-color = 0xFFFFFF
+text_color = 0xFFFFFF
 
 
-def display_password():
+def display_code(code, location_x, location_y):
     splash = displayio.Group()
     display.root_group = splash
-    # Create the tet label
-    text_area = label.Label(font, text="***", color=color)
-    # Set the location
-    text_area.x = 20
-    text_area.y = 25
-    splash.append(text_area)
 
+    # Create text label
+    text_area = label.Label(font, text=code, color=text_color)
 
-def display_pin():
-    splash = displayio.Group()
-    display.root_group = splash
-    # Create the tet label
-    text_area = label.Label(font, text="123", color=color)
-    # Set the location
-    text_area.x = 20
-    text_area.y = 27
+    # Set location
+    text_area.x = location_x
+    text_area.y = location_y
     splash.append(text_area)
 
 
@@ -125,8 +116,8 @@ def init():
         if not btn1.value:
             write_text(password)
             press_enter()
-            display_password()
+            display_code("***", 20, 25)
         if not btn2.value:
             write_text(pin)
             press_enter()
-            display_pin()
+            display_code("123", 20, 27)
