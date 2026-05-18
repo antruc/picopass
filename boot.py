0aa48f9a2d4206b7f92745ccbf8886fcd5db33a7
Antruc <admin@antruc.dev>
2026-05-18 11:37:47 -0600

    Refactor code and improve comments

--- boot.py
+++ boot.py
@@ -5,6 +5,6 @@ btn1 = digitalio.DigitalInOut(board.GP15)
 btn1.direction = digitalio.Direction.INPUT
 btn1.pull = digitalio.Pull.UP
 
-# Disable USB storage only if button is not pressed.
+# Disable USB storage only if button 1 is not pressed
 if btn1.value:
     storage.disable_usb_drive()
